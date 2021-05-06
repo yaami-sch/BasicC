fd14e45785c496aad0f27ef4583b6d27791ada3f
yaami-sch <83202326+yaami-sch@users.noreply.github.com>
2021-05-06 09:35:50 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,10 @@
+main()
+{
+  int i,j sum=0;
+  printf("Enter the values");
+  Scanf("%d %d, &i, &j);
+        sum+i+j;
+        printf("The sum of 2 values is : %d" , sum);
+        getch();
+        }
+        
